8c0efc0e0ee9fdbd4dc370d650c692f5a00c6b90
rusiruavb <rusiruavb98@gmail.com>
2023-09-13 19:27:28 +0530

    feat: add database connection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
+import 'dotenv/config';
 import express from 'express';
+import sequelize from './configs/database.config';
+import configs from './configs/global.config';
 
-const app = express();
-const port = 3000;
+const bootstrap = async () => {
+  const app = express();
+  const { port } = configs.app;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
 
-app.listen(port, () => {
-  console.log(`Express is listening at http://localhost:${port}`);
-});
+  app.listen(port, async () => {
+    try {
+      await sequelize.authenticate();
+      console.log('DB Connected');
+    } catch (error: any) {
+      console.log(error.message);
+    }
+    console.log(`App started at http://localhost:${port}`);
+  });
+};
+
+bootstrap();
